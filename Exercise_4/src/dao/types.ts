439ee181dd1404d72ed5f251bafea85a01936964
Patryk Makowski <praca.makowski@gmail.com>
2021-11-24 12:03:45 +0100

    Change and add types

--- Exercise_4/src/dao/types.ts
+++ Exercise_4/src/dao/types.ts
@@ -16,16 +16,69 @@ export type AdditionalEntityFields = {
   type?: Maybe<Scalars['String']>;
 };
 
+export type Category = {
+  description: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
+
 export type Mutation = {
+  createCategory: Category;
+  createOrder: Order;
+  createProduct: Product;
   createUser: User;
+  updateCategory?: Maybe<Category>;
+  updateOrder?: Maybe<Order>;
+  updateProduct?: Maybe<Product>;
   updateUser?: Maybe<User>;
+};
+
+
+export type MutationCreateCategoryArgs = {
+  description: Scalars['String'];
+  name: Scalars['String'];
+};
+
+
+export type MutationCreateOrderArgs = {
+  products?: Maybe<Array<Product>>;
+  state: OrderState;
+  username: User;
+};
+
+
+export type MutationCreateProductArgs = {
+  category: Category;
+  description: Scalars['String'];
+  name: Scalars['String'];
+  price: Scalars['Float'];
+  weight: Scalars['Int'];
 };
 
 
 export type MutationCreateUserArgs = {
   email: Scalars['String'];
+  phoneNumber?: Maybe<Scalars['Int']>;
   role: Role;
   username: Scalars['String'];
+};
+
+
+export type MutationUpdateCategoryArgs = {
+  categoryId?: Maybe<Scalars['ID']>;
+  data?: Maybe<UpdateCategoryInput>;
+};
+
+
+export type MutationUpdateOrderArgs = {
+  data?: Maybe<UpdateOrderInput>;
+  orderId?: Maybe<Scalars['ID']>;
+};
+
+
+export type MutationUpdateProductArgs = {
+  data?: Maybe<UpdateProductInput>;
+  productId?: Maybe<Scalars['ID']>;
 };
 
 
@@ -38,9 +91,54 @@ export type Node = {
   id: Scalars['ID'];
 };
 
+export type Order = {
+  createdAt: Scalars['String'];
+  id: Scalars['ID'];
+  products?: Maybe<Array<Product>>;
+  state: OrderState;
+  username: User;
+};
+
+export enum OrderState {
+  Approved = 'APPROVED',
+  Cancelled = 'CANCELLED',
+  Fullfield = 'FULLFIELD',
+  NotApproved = 'NOT_APPROVED'
+}
+
+export type Product = {
+  category: Category;
+  description: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  price: Scalars['Float'];
+  weight: Scalars['Int'];
+};
+
 export type Query = {
+  Category?: Maybe<Category>;
+  Order?: Maybe<Order>;
+  Product?: Maybe<Product>;
   User?: Maybe<User>;
+  allCategories: Array<Category>;
+  allOrders: Array<Order>;
+  allProducts: Array<Product>;
   allUsers: Array<User>;
+};
+
+
+export type QueryCategoryArgs = {
+  categoryId: Scalars['ID'];
+};
+
+
+export type QueryOrderArgs = {
+  orderId: Scalars['ID'];
+};
+
+
+export type QueryProductArgs = {
+  productId: Scalars['ID'];
 };
 
 
@@ -53,8 +151,28 @@ export enum Role {
   User = 'USER'
 }
 
+export type UpdateCategoryInput = {
+  description?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+};
+
+export type UpdateOrderInput = {
+  products?: Maybe<Array<Product>>;
+  state?: Maybe<OrderState>;
+  username?: Maybe<User>;
+};
+
+export type UpdateProductInput = {
+  category?: Maybe<Category>;
+  description?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+  price?: Maybe<Scalars['Float']>;
+  weight?: Maybe<Scalars['Int']>;
+};
+
 export type UpdateUserInput = {
   email?: Maybe<Scalars['String']>;
+  phoneNumber?: Maybe<Scalars['String']>;
   role?: Maybe<Scalars['String']>;
   username?: Maybe<Scalars['String']>;
 };
@@ -62,14 +180,39 @@ export type UpdateUserInput = {
 export type User = Node & {
   email: Scalars['String'];
   id: Scalars['ID'];
+  phoneNumber: Scalars['String'];
   role: Role;
   username: Scalars['String'];
 };
 
 import { ObjectId } from 'mongodb';
+export type CategoryDbObject = {
+  description: string,
+  _id: ObjectId,
+  name: string,
+};
+
+export type OrderDbObject = {
+  createdAt: Date,
+  _id: ObjectId,
+  products?: Maybe<Array<ProductDbObject['_id']>>,
+  state: string,
+  username: UserDbObject['_id'],
+};
+
+export type ProductDbObject = {
+  category: CategoryDbObject['_id'],
+  description: string,
+  _id: ObjectId,
+  name: string,
+  price: number,
+  weight: number,
+};
+
 export type UserDbObject = {
   email: string,
   _id: ObjectId,
+  phoneNumber: string,
   role: string,
   username: string,
 };
--- Exercise_4/src/graphql/types.tsx
+++ Exercise_4/src/graphql/types.tsx
@@ -14,16 +14,69 @@ export type Scalars = {
   Float: number;
 };
 
+export type Category = {
+  description: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
+
 export type Mutation = {
+  createCategory: Category;
+  createOrder: Order;
+  createProduct: Product;
   createUser: User;
+  updateCategory?: Maybe<Category>;
+  updateOrder?: Maybe<Order>;
+  updateProduct?: Maybe<Product>;
   updateUser?: Maybe<User>;
+};
+
+
+export type MutationCreateCategoryArgs = {
+  description: Scalars['String'];
+  name: Scalars['String'];
+};
+
+
+export type MutationCreateOrderArgs = {
+  products?: Maybe<Array<Product>>;
+  state: OrderState;
+  username: User;
+};
+
+
+export type MutationCreateProductArgs = {
+  category: Category;
+  description: Scalars['String'];
+  name: Scalars['String'];
+  price: Scalars['Float'];
+  weight: Scalars['Int'];
 };
 
 
 export type MutationCreateUserArgs = {
   email: Scalars['String'];
+  phoneNumber?: Maybe<Scalars['Int']>;
   role: Role;
   username: Scalars['String'];
+};
+
+
+export type MutationUpdateCategoryArgs = {
+  categoryId?: Maybe<Scalars['ID']>;
+  data?: Maybe<UpdateCategoryInput>;
+};
+
+
+export type MutationUpdateOrderArgs = {
+  data?: Maybe<UpdateOrderInput>;
+  orderId?: Maybe<Scalars['ID']>;
+};
+
+
+export type MutationUpdateProductArgs = {
+  data?: Maybe<UpdateProductInput>;
+  productId?: Maybe<Scalars['ID']>;
 };
 
 
@@ -36,9 +89,54 @@ export type Node = {
   id: Scalars['ID'];
 };
 
+export type Order = {
+  createdAt: Scalars['String'];
+  id: Scalars['ID'];
+  products?: Maybe<Array<Product>>;
+  state: OrderState;
+  username: User;
+};
+
+export enum OrderState {
+  Approved = 'APPROVED',
+  Cancelled = 'CANCELLED',
+  Fullfield = 'FULLFIELD',
+  NotApproved = 'NOT_APPROVED'
+}
+
+export type Product = {
+  category: Category;
+  description: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  price: Scalars['Float'];
+  weight: Scalars['Int'];
+};
+
 export type Query = {
+  Category?: Maybe<Category>;
+  Order?: Maybe<Order>;
+  Product?: Maybe<Product>;
   User?: Maybe<User>;
+  allCategories: Array<Category>;
+  allOrders: Array<Order>;
+  allProducts: Array<Product>;
   allUsers: Array<User>;
+};
+
+
+export type QueryCategoryArgs = {
+  categoryId: Scalars['ID'];
+};
+
+
+export type QueryOrderArgs = {
+  orderId: Scalars['ID'];
+};
+
+
+export type QueryProductArgs = {
+  productId: Scalars['ID'];
 };
 
 
@@ -51,8 +149,28 @@ export enum Role {
   User = 'USER'
 }
 
+export type UpdateCategoryInput = {
+  description?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+};
+
+export type UpdateOrderInput = {
+  products?: Maybe<Array<Product>>;
+  state?: Maybe<OrderState>;
+  username?: Maybe<User>;
+};
+
+export type UpdateProductInput = {
+  category?: Maybe<Category>;
+  description?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+  price?: Maybe<Scalars['Float']>;
+  weight?: Maybe<Scalars['Int']>;
+};
+
 export type UpdateUserInput = {
   email?: Maybe<Scalars['String']>;
+  phoneNumber?: Maybe<Scalars['String']>;
   role?: Maybe<Scalars['String']>;
   username?: Maybe<Scalars['String']>;
 };
@@ -60,6 +178,7 @@ export type UpdateUserInput = {
 export type User = Node & {
   email: Scalars['String'];
   id: Scalars['ID'];
+  phoneNumber: Scalars['String'];
   role: Role;
   username: Scalars['String'];
 };
@@ -134,12 +253,21 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
   Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  Category: ResolverTypeWrapper<Category>;
+  Float: ResolverTypeWrapper<Scalars['Float']>;
   ID: ResolverTypeWrapper<Scalars['ID']>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
   Mutation: ResolverTypeWrapper<{}>;
   Node: ResolversTypes['User'];
+  Order: ResolverTypeWrapper<Order>;
+  OrderState: OrderState;
+  Product: ResolverTypeWrapper<Product>;
   Query: ResolverTypeWrapper<{}>;
   Role: Role;
   String: ResolverTypeWrapper<Scalars['String']>;
+  UpdateCategoryInput: UpdateCategoryInput;
+  UpdateOrderInput: UpdateOrderInput;
+  UpdateProductInput: UpdateProductInput;
   UpdateUserInput: UpdateUserInput;
   User: ResolverTypeWrapper<User>;
 };
@@ -147,17 +275,38 @@ export type ResolversTypes = {
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
   Boolean: Scalars['Boolean'];
+  Category: Category;
+  Float: Scalars['Float'];
   ID: Scalars['ID'];
+  Int: Scalars['Int'];
   Mutation: {};
   Node: ResolversParentTypes['User'];
+  Order: Order;
+  Product: Product;
   Query: {};
   String: Scalars['String'];
+  UpdateCategoryInput: UpdateCategoryInput;
+  UpdateOrderInput: UpdateOrderInput;
+  UpdateProductInput: UpdateProductInput;
   UpdateUserInput: UpdateUserInput;
   User: User;
 };
 
+export type CategoryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Category'] = ResolversParentTypes['Category']> = {
+  description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
 export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+  createCategory?: Resolver<ResolversTypes['Category'], ParentType, ContextType, RequireFields<MutationCreateCategoryArgs, 'description' | 'name'>>;
+  createOrder?: Resolver<ResolversTypes['Order'], ParentType, ContextType, RequireFields<MutationCreateOrderArgs, 'state' | 'username'>>;
+  createProduct?: Resolver<ResolversTypes['Product'], ParentType, ContextType, RequireFields<MutationCreateProductArgs, 'category' | 'description' | 'name' | 'price' | 'weight'>>;
   createUser?: Resolver<ResolversTypes['User'], ParentType, ContextType, RequireFields<MutationCreateUserArgs, 'email' | 'role' | 'username'>>;
+  updateCategory?: Resolver<Maybe<ResolversTypes['Category']>, ParentType, ContextType, RequireFields<MutationUpdateCategoryArgs, never>>;
+  updateOrder?: Resolver<Maybe<ResolversTypes['Order']>, ParentType, ContextType, RequireFields<MutationUpdateOrderArgs, never>>;
+  updateProduct?: Resolver<Maybe<ResolversTypes['Product']>, ParentType, ContextType, RequireFields<MutationUpdateProductArgs, never>>;
   updateUser?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<MutationUpdateUserArgs, 'userId'>>;
 };
 
@@ -166,22 +315,51 @@ export type NodeResolvers<ContextType = any, ParentType extends ResolversParentT
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
 };
 
+export type OrderResolvers<ContextType = any, ParentType extends ResolversParentTypes['Order'] = ResolversParentTypes['Order']> = {
+  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  products?: Resolver<Maybe<Array<ResolversTypes['Product']>>, ParentType, ContextType>;
+  state?: Resolver<ResolversTypes['OrderState'], ParentType, ContextType>;
+  username?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type ProductResolvers<ContextType = any, ParentType extends ResolversParentTypes['Product'] = ResolversParentTypes['Product']> = {
+  category?: Resolver<ResolversTypes['Category'], ParentType, ContextType>;
+  description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  price?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
+  weight?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  Category?: Resolver<Maybe<ResolversTypes['Category']>, ParentType, ContextType, RequireFields<QueryCategoryArgs, 'categoryId'>>;
+  Order?: Resolver<Maybe<ResolversTypes['Order']>, ParentType, ContextType, RequireFields<QueryOrderArgs, 'orderId'>>;
+  Product?: Resolver<Maybe<ResolversTypes['Product']>, ParentType, ContextType, RequireFields<QueryProductArgs, 'productId'>>;
   User?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'userId'>>;
+  allCategories?: Resolver<Array<ResolversTypes['Category']>, ParentType, ContextType>;
+  allOrders?: Resolver<Array<ResolversTypes['Order']>, ParentType, ContextType>;
+  allProducts?: Resolver<Array<ResolversTypes['Product']>, ParentType, ContextType>;
   allUsers?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType>;
 };
 
 export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
   email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  phoneNumber?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   role?: Resolver<ResolversTypes['Role'], ParentType, ContextType>;
   username?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
 export type Resolvers<ContextType = any> = {
+  Category?: CategoryResolvers<ContextType>;
   Mutation?: MutationResolvers<ContextType>;
   Node?: NodeResolvers<ContextType>;
+  Order?: OrderResolvers<ContextType>;
+  Product?: ProductResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
 };
